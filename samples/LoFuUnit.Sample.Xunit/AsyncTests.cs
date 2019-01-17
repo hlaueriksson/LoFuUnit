3d01597f1d1233833ecfa45e0853c8b3500578a4
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2019-01-17 21:52:52 +0100

    :recycle:

--- samples/LoFuUnit.Sample.Xunit/AsyncTests.cs
+++ samples/LoFuUnit.Sample.Xunit/AsyncTests.cs
@@ -1,14 +1,17 @@
 ﻿using System.Net.Http;
 using System.Threading.Tasks;
 using FluentAssertions;
+using LoFuUnit.Xunit;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace LoFuUnit.Sample.Xunit
 {
-    public class AsyncTests : LoFuUnit.Xunit.LoFuTest
+    public class AsyncTests
     {
-        public AsyncTests(ITestOutputHelper output) : base(output) { }
+        private ITestOutputHelper Output { get; }
+
+        public AsyncTests(ITestOutputHelper output) => Output = output;
 
         private HttpClient Subject { get; set; }
         private HttpResponseMessage Response { get; set; }
@@ -16,10 +19,10 @@ namespace LoFuUnit.Sample.Xunit
         [Fact]
         public async Task HttpClient()
         {
-            await AssertAsync();
+            await this.AssertAsync(Output);
 
             void given_a_HttpClient() => Subject = new HttpClient();
-            async Task when_get_the_GitHub_site() => Response = await Subject.GetAsync("https://github.com");
+            async Task when_getting_the_GitHub_site() => Response = await Subject.GetAsync("https://github.com");
             void then_it_should_have_success_status_code() => Response.EnsureSuccessStatusCode();
             async Task then_it_should_have_proper_content()
             {
