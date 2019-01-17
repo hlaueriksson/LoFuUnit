018a9036da70c3b1767fc9a5a8ffa8d3d5a38ee4
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2019-01-17 21:04:30 +0100

    :information_desk_person: LoFuUnit.Sample.Xunit

---
+++ samples/LoFuUnit.Sample.Xunit/AsyncTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LoFuUnit.Sample.Xunit
+{
+    public class AsyncTests : LoFuUnit.Xunit.LoFuTest
+    {
+        public AsyncTests(ITestOutputHelper output) : base(output) { }
+
+        private HttpClient Subject { get; set; }
+        private HttpResponseMessage Response { get; set; }
+
+        [Fact]
+        public async Task HttpClient()
+        {
+            await AssertAsync();
+
+            void given_a_HttpClient() => Subject = new HttpClient();
+            async Task when_get_the_GitHub_site() => Response = await Subject.GetAsync("https://github.com");
+            void then_it_should_have_success_status_code() => Response.EnsureSuccessStatusCode();
+            async Task then_it_should_have_proper_content()
+            {
+                var content = await Response.Content.ReadAsStringAsync();
+                content.Should().Contain("Built for developers");
+            }
+        }
+    }
+}
---
+++ samples/LoFuUnit.Sample.Xunit/TestsWithDispose.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using LoFuUnit.Xunit;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace LoFuUnit.Sample.Xunit
+{
+    public class TestsWithDispose : IDisposable
+    {
+        private ITestOutputHelper Output { get; }
+
+        public TestsWithDispose(ITestOutputHelper output) => Output = output;
+
+        public void Dispose() => this.Assert(Output as TestOutputHelper);
+
+        private Stack<int> Subject { get; set; }
+
+        [Fact]
+        public void Empty_stack()
+        {
+            Subject = new Stack<int>();
+
+            void should_have_no_elements() => Subject.Count.Should().Be(0);
+            void should_throw_an_exception_when_calling__Pop__() => Subject.Invoking(y => y.Pop()).Should().Throw<InvalidOperationException>();
+            void should_throw_an_exception_when_calling__Peek__() => Subject.Invoking(y => y.Peek()).Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void Non_empty_stack()
+        {
+            Subject = new Stack<int>(new[] { 1, 2, 3 });
+
+            void should_return_the_top_element_when_calling__Peek__() => Subject.Peek().Should().Be(3);
+            void should_not_remove_the_top_element_when_calling__Peek__()
+            {
+                Subject.Peek();
+                Subject.Should().Contain(3);
+            }
+            void should_return_the_top_element_when_calling__Pop__() => Subject.Pop().Should().Be(3);
+            void should_remove_the_top_element_when_calling__Pop__()
+            {
+                Subject.Pop();
+                Subject.Should().NotContain(3);
+            }
+        }
+    }
+}
