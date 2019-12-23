b1477f003cd2251f0ca3ad3ad74ed207b5a01344
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2019-12-23 15:00:52 +0100

    docs

--- src/LoFuUnit/InconclusiveTestMethodException.cs
+++ src/LoFuUnit/InconclusiveTestMethodException.cs
@@ -2,8 +2,15 @@
 
 namespace LoFuUnit
 {
+    /// <summary>
+    /// The exception that is thrown when a test method is inconclusive.
+    /// </summary>
     public class InconclusiveTestMethodException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InconclusiveTestMethodException" /> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
         public InconclusiveTestMethodException(string message) : base(message)
         {
         }
