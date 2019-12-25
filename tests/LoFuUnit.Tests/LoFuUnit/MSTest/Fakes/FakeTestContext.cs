508f8dfb153305af1a37a836109e207e375d6327
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2019-12-25 19:58:57 +0100

    bump test and sample nugets

--- tests/LoFuUnit.Tests/LoFuUnit/MSTest/Fakes/FakeTestContext.cs
+++ tests/LoFuUnit.Tests/LoFuUnit/MSTest/Fakes/FakeTestContext.cs
@@ -1,13 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 #if NETFRAMEWORK
-using System.Collections;
 using System.Data;
 using System.Data.Common;
-#endif
-
-#if NETCOREAPP
-using System.Collections.Generic;
 #endif
 
 namespace LoFuUnit.Tests.LoFuUnit.MSTest.Fakes
@@ -16,30 +12,24 @@ namespace LoFuUnit.Tests.LoFuUnit.MSTest.Fakes
     {
         public override string TestName { get; }
 
+        public override IDictionary Properties { get; }
+
         public FakeTestContext(string methodName) => TestName = methodName;
 
-#if NETFRAMEWORK
+        public override void AddResultFile(string fileName) => throw new System.NotImplementedException();
+
         public override void WriteLine(string message) => throw new System.NotImplementedException();
 
         public override void WriteLine(string format, params object[] args) => throw new System.NotImplementedException();
 
-        public override void AddResultFile(string fileName) => throw new System.NotImplementedException();
-
+#if NETFRAMEWORK
         public override void BeginTimer(string timerName) => throw new System.NotImplementedException();
 
         public override void EndTimer(string timerName) => throw new System.NotImplementedException();
 
-        public override IDictionary Properties { get; }
         public override DataRow DataRow { get; }
+
         public override DbConnection DataConnection { get; }
-#endif
-
-#if NETCOREAPP
-        public override void WriteLine(string message) => throw new System.NotImplementedException();
-
-        public override void WriteLine(string format, params object[] args) => throw new System.NotImplementedException();
-
-        public override IDictionary<string, object> Properties { get; }
 #endif
     }
 }
