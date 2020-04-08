4ecdd88528335ec3783ad39b58fb36a462960b3b
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2020-04-08 18:42:55 +0200

    update base class for Xunit

--- src/LoFuUnit.Xunit/LoFuTest.cs
+++ src/LoFuUnit.Xunit/LoFuTest.cs
@@ -1,11 +1,14 @@
-﻿using Xunit.Abstractions;
+﻿using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
 
 namespace LoFuUnit.Xunit
 {
     /// <summary>
     /// Base class for <c>Xunit</c> test fixtures.
     /// </summary>
-    public abstract class LoFuTest : LoFuUnit.LoFuTest
+    public abstract class LoFuTest : LoFuUnit.LoFuTest, IAsyncLifetime
     {
         /// <summary>
         /// Test output log writer.
@@ -16,10 +19,21 @@ namespace LoFuUnit.Xunit
         /// Initializes a new instance of the test fixture with a <see cref="ITestOutputHelper"/>.
         /// </summary>
         /// <param name="output">A test output log writer</param>
-        protected LoFuTest(ITestOutputHelper output)
-        {
-            Output = output;
-        }
+        protected LoFuTest(ITestOutputHelper output) => Output = output;
+
+        /// <summary>
+        /// Does nothing.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        /// <remarks>Override this method to change how the test setup is done.</remarks>
+        public virtual async Task InitializeAsync() => await Task.CompletedTask;
+
+        /// <summary>
+        /// Runs the local functions in the containing test method that just executed.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        /// <remarks>Override this method to change how the test cleanup is done.</remarks>
+        public virtual async Task DisposeAsync() => await this.AssertAsync(Output as TestOutputHelper);
 
         /// <summary>
         /// Writes the specified message, followed by the current line terminator, to the test output stream.
