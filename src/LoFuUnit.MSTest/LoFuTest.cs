b0573af29ee01db7d7e329311a5a1a41b2dc5da4
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2020-04-08 18:42:36 +0200

    base class for MSTest

---
+++ src/LoFuUnit.MSTest/LoFuTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LoFuUnit.MSTest
+{
+    /// <summary>
+    /// Base class for <c>MSTest</c> test fixtures.
+    /// </summary>
+    public abstract class LoFuTest : LoFuUnit.LoFuTest
+    {
+        /// <summary>
+        /// Stores information that is provided to unit tests.
+        /// </summary>
+        public TestContext TestContext { get; set; }
+
+        /// <summary>
+        /// Runs the local functions in the containing test method that just executed.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        /// <remarks>Override this method to change how the test cleanup is done.</remarks>
+        [TestCleanup]
+        public virtual async Task TestCleanup() => await this.AssertAsync(TestContext);
+    }
+}
