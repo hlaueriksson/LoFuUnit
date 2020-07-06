e93f74b7a294666a0db9922ad7eb521e665a2598
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2020-07-06 21:03:27 +0200

    Create LoFuTestMethodAttribute.cs

---
+++ src/LoFuUnit.MSTest/LoFuTestMethodAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LoFuUnit.MSTest
+{
+    public class LoFuTestMethodAttribute : TestMethodAttribute
+    {
+        public override TestResult[] Execute(ITestMethod testMethod)
+        {
+            // TODO: get fixture how?
+
+            return new[] { testMethod.Invoke(null) };
+        }
+    }
+}
