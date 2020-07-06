5ff78ba5ce695dbd439a0aef6d5a45ee402344fd
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2020-07-06 21:05:01 +0200

    Create LoFuAttribute.cs

---
+++ src/LoFuUnit.Xunit/LoFuAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Xunit.Sdk;
+
+namespace LoFuUnit.Xunit
+{
+    public class LoFuAttribute : BeforeAfterTestAttribute
+    {
+        public override void Before(MethodInfo methodUnderTest)
+        {
+        }
+
+        public override void After(MethodInfo methodUnderTest)
+        {
+            // TODO: get fixture how?
+        }
+    }
+}
